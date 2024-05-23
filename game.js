02b68ade2e5661127a316ad881e92321132fe774
Kevin <kevin.cano@ynov.com>
2024-05-23 14:28:29 +0200

    Can use ZQSD keys

--- game.js
+++ game.js
@@ -46,22 +46,29 @@ function create() {
     this.physics.add.collider(this.player, ground);
 
     this.cursors = this.input.keyboard.createCursorKeys();
+    
+    this.keys = this.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.Z,
+        left: Phaser.Input.Keyboard.KeyCodes.Q,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
 }
 
 function update() {
     this.player.setVelocityX(0);
 
-    if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown || this.keys.left.isDown) {
         this.player.setVelocityX(-playerConfig.playerSpeed);
-    } else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown || this.keys.right.isDown) {
         this.player.setVelocityX(playerConfig.playerSpeed);
     }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
+    if ((this.cursors.up.isDown || this.keys.up.isDown) && this.player.body.touching.down) {
         this.player.setVelocityY(playerConfig.playerJump);
     }
 
-    if (this.cursors.down.isDown) {
+    if (this.cursors.down.isDown || this.keys.down.isDown) {
         this.player.setVelocityY(playerConfig.playerSpeed);
     }
 }
