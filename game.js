febe324ced8b01228e9c299b2528103672e3f51a
nsonita <sonita.nou@ynov.com>
2024-05-23 11:52:49 +0200

    Assets + Mise en place

--- game.js
+++ game.js
@@ -1,11 +1,34 @@
-var config = {
+let config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 576,
-    backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
-  }
-  
-  
-  var game = new Phaser.Game(config);
-  
+    scale: {
+        width: 1370,
+        height: innerHeight,
+    },
+    backgroundColor: '#575555',
+    
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+}
+
+let game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("BigBob", "assets/BigBob.png");
+}
+
+function create() {
+    W = game.config.width;
+    H = game.config.height;
+    let ground = this.add.tileSprite(0, H - 48, W, 48, "ground");
+    ground.setOrigin(0, 0);
+
+    this.player = this.add.sprite(40, 540, 'BigBob', 8);
+}
+
+function update() {
+
+}
