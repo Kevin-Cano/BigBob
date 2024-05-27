51ee5cfbd2bc64ebac756251400182c99c284b11
nsonita <sonita.nou@ynov.com>
2024-05-27 23:11:01 +0200

    Système de score + ennemi + UI

--- README.md
+++ README.md
@@ -16,11 +16,15 @@ ou en tapant cette commande dans un powershell :
 winget install OpenJS.NodeJS
 ```
 
-- L'extension "Live server" sur votre VSCode afin de faciliter l'accès au jeu.
+- L'extension `Live server` sur votre VSCode afin de faciliter l'accès au jeu.
 
 ## Comment utiliser notre projet ?
 
 Il vous suffit de lancer votre VSCode, d'ouvrir le dossier que vous pourrez cloner [ici](https://ytrack.learn.ynov.com/git/cakevin/eval-js.git) et de cliquer sur "Go Live" en bas a droite de votre écran.
+
+## Règles du jeu
+
+Le but du jeu est de récupérer tous les sandwichs sur une carte en 2D en traversant diverses plateformes. Les commandes `flèches` et `ZQSD` servent pour le déplacement du joueur. Il peut aussi utiliser un "dash" avec la commande `Shift` afin de faire un print rapide d'une seconde vers la droite ou la gauche. La partie est terminée une fois que le joueur a récupéré tous les sandwichs. Il existe 15 sandwichs sur la carte. Le score est affiché en bas à gauche de l'écran.
 
 # <div align="right"> Crédits
 
--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-let config = {
+let config = { //mise en place de la scène
     type: Phaser.AUTO,
     scale: {
         width: 1500,
-        height: window.innerHeight, // Ajustement de la hauteur en fonction de la fenêtre
+        height: window.innerHeight,
     },
     backgroundColor: '#575555',
     physics: {
@@ -24,13 +24,17 @@ let playerConfig = {
     playerSpeed: 200,
     playerJump: -575,
     dashSpeed: 600,
-    dashDuration: 200, 
-    dashCooldown: 1000 
-}
+    dashDuration: 200,
+    dashCooldown: 1000,
+};
+
+
+let score = 0;
+let sandwichesRemaining = 15;
 
 let game = new Phaser.Game(config);
 
-function preload() {
+function preload() { //chargement de tous les assets
     this.load.image("ground", "assets/ground.png");
     this.load.image("BigBob", "assets/BigBob.png");
     this.load.image("bloc", "assets/bloc.png");
@@ -38,9 +42,10 @@ function preload() {
     this.load.atlas('ui', 'assets/nine-slice.png', 'assets/nine-slice.json');
     this.load.image("prisonner", 'assets/prisonner.png');
     this.load.image("grille", 'assets/grille.png');
+    this.load.image("villain", "assets/villain.png");
 }
 
-function create() {
+function create() { // création de tous les décors avec des collisions et aussi du joueur et du pnj
     W = game.config.width;
     H = game.config.height;
     let grille = this.add.group({
@@ -59,14 +64,14 @@ function create() {
     })
     let grilleH = this.add.group({
         key: "grille",
-        repeat: 50,
+        repeat: 70,
         setScale: { x: 1, y: 2 },
         setXY: { x: 44, y: 260, stepX: 78 },
 
     })
 
 
-    let grilleSolo = this.add.sprite(44, 522, "grille").setScale(1, 2)
+    let grilleSolo = this.add.sprite(44, 522, "grille").setScale(1, 2) // ajout des différents décors
     let prisonner = this.add.sprite(122, 522, "prisonner").setScale(1, 1)
     let prisonner2 = this.add.sprite(1058, 522, "prisonner").setScale(1, 1)
     
@@ -74,15 +79,23 @@ function create() {
     ground.setOrigin(0, 0);
     this.player = this.physics.add.sprite(40, 50, 'BigBob'); 
 
+    this.villain = this.physics.add.sprite(1300, 50, 'villain');
+    this.villainDirection = 'right';
+    this.villainSpeed = 100;
+
     this.physics.add.existing(ground, true);
     
     this.player.setCollideWorldBounds(true);
 
+    this.villain.setCollideWorldBounds(true);
+
     this.physics.add.collider(this.player, ground);
+    this.physics.add.collider(this.villain, ground);
+    this.physics.add.collider(this.player, this.villain);
 
     this.cursors = this.input.keyboard.createCursorKeys();
     
-    this.keys = this.input.keyboard.addKeys({
+    this.keys = this.input.keyboard.addKeys({ // contrôles du jeu
         up: Phaser.Input.Keyboard.KeyCodes.Z,
         left: Phaser.Input.Keyboard.KeyCodes.Q,
         down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -90,32 +103,39 @@ function create() {
         shift: Phaser.Input.Keyboard.KeyCodes.SHIFT
     });
 
-
-
-    let heal = this.physics.add.group({
+    let heal = this.physics.add.group({ // mise en place des sandwichs
         key: "sandwich",
-        repeat: 20,
+        repeat: 15,
         setScale: { x: 1, y: 1 },
         setXY: { x: 200, y: 100, stepX: 350 },
+    });
 
-    })
+    this.uiContainer = this.add.container(0, 0);
 
-    const bar1 = this.add.nineslice(150, 170, 'ui', 'yellow_button06');
-        const fill1 = this.add.nineslice(20, 170, 'ui', 'yellow_button07', 13, 99, 1);
+    const bar1 = this.add.nineslice(300, 120, 'ui', 'yellow_button06'); // barre de vie pas fonctionnelle
+    const fill1 = this.add.nineslice(170, 120, 'ui', 'yellow_button07', 13, 99, 1);
+    fill1.setOrigin(0, 0.5);
 
-        fill1.setOrigin(0, 0.5);
-        this.tweens.add({
-            targets: fill1,
-            width: 258,
-            duration: 3000,
-        });
+    this.uiContainer.add([bar1, fill1]);
+
+    this.rules = this.add.text(50, 300, 'Récupérez tous les sandwichs !', { fontFamily: 'Courier New',fontSize: '32px', fill: '#fff' });
+
+    this.scoreText = this.add.text(150, 600, 'Score: 0', { fontFamily: 'Courier New',fontSize: '32px', fill: '#fff' });
+    this.scoreText.setScrollFactor(0);
+
+    let lifeText = this.add.text(300, 120, 'Extra Mayo', { fontFamily: 'Courier New', fontSize: '24px', fill: '#ffffff' });
+    lifeText.setOrigin(0.5);
+    this.uiContainer.add(lifeText); 
+
+    this.tweens.add({
+        targets: fill1,
+        width: 258,
+    });
 
     let blocs = this.physics.add.staticGroup();
 
     heal.create(500, H - 500, "sandwich").refreshBody();
 
-
-    
     // Platforme de départ
     blocs.create(250, H - 100, "bloc").refreshBody();
     blocs.create(300, H - 100, "bloc").refreshBody();
@@ -170,7 +190,7 @@ function create() {
     let platforms = this.physics.add.staticGroup();
     platforms.add(ground);
 
-    this.physics.add.collider(platforms, this.player);
+    this.physics.add.collider(platforms, this.player); // toutes les collisions possibles
     this.physics.add.collider(this.player, blocs);
 
     this.physics.add.collider(platforms, heal);
@@ -181,7 +201,7 @@ function create() {
     this.cameras.main.setBounds(0, 0, 5000, H); 
     this.physics.world.setBounds(0, 0, 5000, H); 
 
-    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05); // caméra qui centre le joueur tout au long de la partie
     this.cameras.main.setZoom(1.2);
 
     this.lastLeftPressTime = 0;
@@ -189,12 +209,9 @@ function create() {
     this.lastDash = 0;
     this.isDashing = false;
     this.dashEndTime = 0;
-
-    
-
 }
 
-function update(time, delta) {
+function update(time, delta) { // mise à jour de diverses possibilités
     let currentTime = this.time.now;
 
     if (this.isDashing && currentTime >= this.dashEndTime) {
@@ -211,7 +228,21 @@ function update(time, delta) {
         }
     }
 
-    function handleMovement(currentTime) {
+    
+
+    if (this.villainDirection === 'right') { // déplacement du vilain qui fait des rondes sur des cases précises
+        this.villain.setVelocityX(this.villainSpeed);
+        if (this.villain.x >= 1500) {
+            this.villainDirection = 'left';
+        }
+    } else {
+        this.villain.setVelocityX(-this.villainSpeed);
+        if (this.villain.x <= 1300) {
+            this.villainDirection = 'right';
+        }
+    }
+
+    function handleMovement(currentTime) { // configuration des dash
         let velocityX = 0;
 
         if (this.cursors.left.isDown || this.keys.left.isDown) {
@@ -239,8 +270,24 @@ function update(time, delta) {
             this.lastDash = currentTime;
         }
     }
+
+    this.uiContainer.setPosition(this.cameras.main.scrollX, this.cameras.main.scrollY);
 }
 
-function eatSandwich(player, sandwich) {
-    sandwich.disableBody(true, true)
+function eatSandwich(player, sandwich) { // système de score grâce aux sandwichs
+    sandwich.disableBody(true, true);
+    sandwichesRemaining--;
+    score += 10;
+
+    this.scoreText.setText('Score: ' + score);
+
+    if (sandwichesRemaining === 0) {
+        endGame.call(this);
+    }
+}
+
+function endGame() { //fin de la partie après que le joueur ait trouvé les 15 sandwichs
+    let victoryMessage = this.add.text(game.config.width / 2, game.config.height / 2, 'Vous avez gagné !', { fontFamily: 'Courier New', fontSize: '48px', fill: '#fff' });
+    victoryMessage.setOrigin(0.5);
+    victoryMessage.setScrollFactor(0);
 }
